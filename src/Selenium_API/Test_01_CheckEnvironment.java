d752a15bd66f63cfa379b361a340377c41192436
Tien Huynh <Tien Huynh@DESKTOP-MAF25M2.net.fpt>
2018-07-14 18:37:20 +0700

    Add conmmit the first

---
+++ src/Selenium_API/Test_01_CheckEnvironment.java
@@ -0,0 +1,37 @@
+package Selenium_API;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Test_01_CheckEnvironment {
+	WebDriver driver;
+	
+  @Test
+  public void f() {
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle, "Guru99 Bank Home Page");
+	  
+	  String homePageUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new ChromeDriver();
+	  driver.get("http://demo.guru99.com/v4/");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
